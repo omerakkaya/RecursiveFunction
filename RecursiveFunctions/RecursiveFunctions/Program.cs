b9b88de6626a67b128e34c254c339d1f323d8f1a
omerakkaya <74490637+omerakkaya@users.noreply.github.com>
2021-06-19 12:13:32 +0300

    Add files via upload

---
+++ RecursiveFunctions/RecursiveFunctions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n'yi girin : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("r'yi girin : ");
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Sonuç = " + Permutasyon(n, r));
+            Console.ReadKey();
+        }
+
+        public static int Permutasyon(int n, int r)
+        {
+            if (r == 0)
+                return 1;
+
+            return n * Permutasyon(n - 1, r - 1);
+        }
+    }
+}
